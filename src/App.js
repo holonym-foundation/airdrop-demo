b47c3322bc806588d6a91d23f47e77f7e4840d3d
nanaknihal <nanaknihal@gmail.com>
2022-10-22 01:09:34 -0400

    add wallet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Step } from './components/Step';
+// Switch to the right network:
+window.ethereum.request({
+  method: "wallet_addEthereumChain",
+  params: [
+    {
+      chainId: "0x1a4",
+      rpcUrls: ["https://goerli.optimism.io/"],
+      chainName: "Optimism Goerli Testnet",
+      nativeCurrency: {
+        name: "ETH",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      blockExplorerUrls: ["https://goerli-optimism.etherscan.io"],
+    },
+  ],
+});
+
 function App() {
+  const [address, setAddress] = useState(null);
+  useEffect(()=>{
+    async function f() {
+      const address = (await window.ethereum.request({ method: "eth_requestAccounts" }))[0]
+      setAddress(address);
+      console.log(address);
+    }
+    f();
+  })
+    
   return (
     <div className='bg'>
+      <code>address: {address}</code>
       <h1>Collect your fair airdrop</h1>
       <Step title='Step 1: Download Holonym Extension' complete={true}>
       <a target='_blank' className='step button' href='https://chrome.google.com/webstore/detail/holonym/obhgknpelgngeabaclepndihajndjjnb'>Download</a>
